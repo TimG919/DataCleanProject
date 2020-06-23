67c472652bd3e0c4b742a4d2b9ead253f2dc83ff
TimG919 <tim101okla@gmail.com>
2020-06-23 16:59:28 -0500

    final versions

--- run_analysis.R
+++ run_analysis.R
@@ -1,13 +1,8 @@
 library(data.table)
 
-    1.Merges the training and the test sets to create one data set.
-    2.Extracts only the measurements on the mean and standard deviation for each measurement.
-    3.Uses descriptive activity names to name the activities in the data set
-    4.Appropriately labels the data set with descriptive variable names.
-    5.From the data set in step 4, creates a second, independent tidy data set with the average of each variable for each activity and each subject.
-
     #   1.Merges the training and the test sets to create one data set.
 # read train data sets and combine
+
 subjectTest <- fread("./UCI HAR Dataset/test/subject_test.txt",col.names = "subject")
 xTest <- fread("./UCI HAR Dataset/test/X_test.txt")
 yTest <- fread("./UCI HAR Dataset/test/y_test.txt", col.names = "activity")
